428f65bcc9dbbb0b344d3e556644139d20d0654d
Artem Abramov <notdepot@gmail.com>
2026-08-03 12:51:43 +0000

    Draft commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,20 +13,7 @@ set -a
 . /etc/hidden/.env
 set +a
 
-# (
-#   while true; do
-#     sleep "$GOCRYPTFS_WATCHDOG_INTERVAL_SECONDS"
-#     cd /opt/hidden && python3 -m app.runtime.watchdog
-#   done
-# ) >> /proc/1/fd/1 2>> /proc/1/fd/2 &
-
-if [ -x /opt/hidden/.venv/bin/uvicorn ]; then
-  UVICORN_BIN=/opt/hidden/.venv/bin/uvicorn
-else
-  UVICORN_BIN=uvicorn
-fi
-
-exec "$UVICORN_BIN" app.main:app \
+exec uvicorn app.main:app \
   --host "$UVICORN_HOST" \
   --port "$UVICORN_PORT" \
   --workers 1
