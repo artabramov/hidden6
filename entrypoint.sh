c96b72aa4a299f5458036505ea9e450149f408cc
Artem Abramov <notdepot@gmail.com>
2026-08-07 14:54:28 +0000

    Naming

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,7 +24,7 @@ mkdir -p \
 
 (
   while true; do
-    sleep "$GOCRYPTFS_WATCHDOG_INTERVAL_SECONDS"
+    sleep "$WATCHDOG_INTERVAL_SECONDS"
     cd "$INSTALL_SOURCE_CODE" && python3 -m app.runtime.watchdog
   done
 ) >> /proc/1/fd/1 2>> /proc/1/fd/2 &
